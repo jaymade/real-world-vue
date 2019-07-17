8a0aaab75eebdd7e3258759e989fa74a6b8456de
Jay <jaymade@Blinky.jaymade>
2019-07-16 22:19:02 -0400

    nested components, navbar and event card

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="event-list">
     <h1>Events List</h1>
-    <router-link :to="{ name: 'event-show', params: { id: '1'} }">Event #1</router-link>
+    <EventCard />
   </div>
 </template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
